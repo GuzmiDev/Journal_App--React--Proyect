2c9347f1f7c9335538dcc0337bd78f0dbce49d6f
Erick Guzman <guzmi.dev@gmail.com>
2022-04-18 16:47:43 -0600

    :art: actions auth

--- src/componentes/auth/LoginScreen.jsx
+++ src/componentes/auth/LoginScreen.jsx
@@ -1,16 +1,35 @@
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { startLoginEmailPassword } from "../../actions/auth";
+import { useForm } from "../../hooks/useForm";
 
 const LoginScreen = () => {
+  const dispatch = useDispatch();
+
+  const [formValues, handleInputChange] = useForm({
+    email: "guzmi.dev@gmail.com",
+    password: "123456",
+  });
+
+  const { email, password } = formValues;
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    dispatch(startLoginEmailPassword(email, password));
+  };
+
   return (
     <>
       <h3 className="auth__title mb-5">Login</h3>
-      <form>
+      <form onSubmit={handleLogin}>
         <input
           className="auth__input"
           type="text"
           placeholder="email"
           name="email"
           autoComplete="off"
+          value={email}
+          onChange={handleInputChange}
         />
         <input
           className="auth__input"
@@ -18,6 +37,8 @@ const LoginScreen = () => {
           placeholder="Password"
           name="password"
           autoComplete="off"
+          value={password}
+          onChange={handleInputChange}
         />
         <button className="btn btn-primary btn-block" type="submit">
           Login
